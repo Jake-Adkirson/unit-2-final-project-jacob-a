8a9bff08114007a697ae603816dcabc766a70cfb
Jake Adkirson <jikjaklol363@gmail.com>
2025-10-17 15:40:04 -0500

    Switched repositories to interfaces from classes.

--- backend/src/main/java/com/example/demo/repositories/EventRepository.java
+++ backend/src/main/java/com/example/demo/repositories/EventRepository.java
@@ -3,6 +3,6 @@ package com.example.demo.repositories;
 import com.example.demo.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
--- backend/src/main/java/com/example/demo/repositories/UserRepository.java
+++ backend/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repositories;
 import com.example.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
